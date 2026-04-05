6b3c116ae571ea767a1a152f4473ca6ce1413b78
Tran Quoc Huy <tryhardlvc0602@gmail.com>
2026-04-05 14:13:13 +0700

    update choosing music buttons

--- include/app_settings.h
+++ include/app_settings.h
@@ -2,9 +2,10 @@
 #include "raylib.h"
 
 struct AppSettings {
-    float musicVolume = 0.4f;
-    float sfxVolume   = 0.7f;
-    int   bgColorIdx  = 0;
+    float musicVolume  = 0.4f;
+    float sfxVolume    = 0.7f;
+    int   bgColorIdx   = 0;
+    int   musicTrackIdx = 1;   // 0 = no music, 1 = music1, 2 = music2
 
     static const Color  bgColors[];
     static const char*  bgColorNames[];
