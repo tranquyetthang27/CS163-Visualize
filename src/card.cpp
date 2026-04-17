310e0a32a2d96fa4504ace3a0576a84d68b6c909
unknown <tquyetthang2k7@gmail.com>
2026-04-17 17:28:28 +0700

    feat: update

--- src/card.cpp
+++ src/card.cpp
@@ -9,7 +9,7 @@ Card::Card(Rectangle r, Screen t,
       accent(acc), hoverAnim(0.0f), animTime(0.0f) {}
 
 bool Card::Update() {
-    bool hovered = false;
+    bool hovered = CheckCollisionPointRec(GetMousePosition(), rect);
     hoverAnim += ((hovered ? 1.0f : 0.0f) - hoverAnim) * 10.0f * GetFrameTime();
     if (hoverAnim < 0.00f)
         animTime += GetFrameTime();
