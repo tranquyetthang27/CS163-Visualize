df75a73a2610d04e66d937998f28055c34972c06
Thanh Loi <thanhtranloi1912@gmail.com>
2026-04-28 14:47:25 +0700

    test automatic capitalization init_file trie

--- include/camera.h
+++ include/camera.h
@@ -3,13 +3,12 @@
 
 #include "raylib.h"
 
-// Khoi tao camera mac dinh
+
 Camera2D InitSceneCamera();
 
-// Xu ly thu phong tai vi tri con tro chuot
+
 void UpdateCameraZoom(Camera2D &camera);
 
-// Xu ly di chuyen man hinh bang chuot phai
 void UpdateCameraPan(Camera2D &camera);
 
 #endif
